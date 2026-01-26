a2f31f946a2fd174b77bc0cfe34c8ad3807ea0b6
Abisin <abisin@example.com>
2026-01-26 11:57:52 +0530

    delete logs

--- run_emulator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export JAVA_HOME="$HOME/development/jdk-17"
-export ANDROID_SDK_ROOT="$HOME/development/android-sdk"
-export ANDROID_AVD_HOME="$HOME/.android/avd"
-export PATH="$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/cmdline-tools/latest/bin:$PATH"
-
-echo "Starting Flutter Emulator..."
-$ANDROID_SDK_ROOT/emulator/emulator -avd flutter_emulator -netdelay none -netspeed full &
